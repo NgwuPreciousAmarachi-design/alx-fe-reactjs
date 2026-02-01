9b6791a45d19dec5d45709f6f601eb26db0effa8
NgwuPreciousAmarachi-design <ngwupreciousa2@gmail.com>
2026-02-01 04:56:28 -0800

    Added Recipe sharing app

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,17 +1,22 @@
-  // RecipeList component
   import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom';
 
-  const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.recipes);
+const RecipeList = () => {
+  // Use filteredRecipes if search is active
+  const recipes = useRecipeStore(state => 
+    state.searchTerm ? state.filteredRecipes : state.recipes
+  );
 
-    return (
-      <div>
-        {recipes.map(recipe => (
-          <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))}
-      </div>
-    );
-  };
+  return (
+    <div>
+      {recipes.map(recipe => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+          <Link to={`/recipe/${recipe.id}`}>View Details</Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default RecipeList;
